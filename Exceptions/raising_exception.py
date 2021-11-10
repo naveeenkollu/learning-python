1b9d6da5da8ea9ad00ba12d451d9734da1d23f2e
Naveen Kollu <naveenkollu10@gmail.com>
2021-11-10 12:38:17 +0530

    Initial commit

--- Exceptions/raising_exception.py
+++ Exceptions/raising_exception.py
@@ -1,7 +1,7 @@
 def calcuate_xfactor(age):
     if age <= 0:
         raise ValueError("Age cannot be 0 or less.")
-        # Raising an exception when the argument passed to this function is zero or less than zero. 
+        # This exception is raised when the age is 0 or less than zero.
         
     return 10 / age
 
