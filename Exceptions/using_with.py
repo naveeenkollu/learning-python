44819f6623d64d2905a1c3f74d8533ae3e8397b8
Naveen Kollu <naveenkollu10@gmail.com>
2021-11-10 12:09:17 +0530

    Initial commit

---
+++ Exceptions/using_with.py
@@ -0,0 +1,14 @@
+try:
+    with open("app.py") as file:
+        print("File opened.")
+    # To avoid using 'finally', I can use 'with' clause with automatically opens and closes an external resource 
+    # after execution.
+
+    age = int(input("Age: "))
+
+except ValueError:
+    print("Enter valid input type of 'int'")
+
+else:
+    print("Executed perfectly!")
+
